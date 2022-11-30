4f64f74ea420a92aa4d768e018bd2eb1e2df4aca
iranika <7370486+iranika@users.noreply.github.com>
2022-11-30 14:10:38 +0900

    fix image dir url

--- mops/generate-4komaDataJs.ps1
+++ mops/generate-4komaDataJs.ps1
@@ -4,7 +4,8 @@ param (
     [string]
     $InputFile = "./4komaData.json"
     ,$OutFile = "./4komaData.js"
-    ,$JsMinFile = "./4komaDataMin.js"
+    ,$JsMinFile = "./4komaDataMin.js",
+    $ImageDirUrl = "https://mo-4koma.iranika.info/4koma/ja/"
 
 )
 
@@ -13,7 +14,8 @@ $json = Get-Content $InputFile | ConvertFrom-Json
 $js = $json | % {
     #$result = $_
     $_.ImagesUrl = $_.ImagesUrl | % {
-        return (New-Object System.Uri((New-Object System.Uri("https://mo4koma.iranika.info/4koma/ja/")), $_)).AbsoluteUri
+        # ImageDirUrl + ImageFileName => AbsoluteUri
+        return (New-Object System.Uri((New-Object System.Uri($ImageDirUrl)), $_)).AbsoluteUri
     }
     if ($_.ImagesUrl -is [System.String]){
         $_.ImagesUrl = [System.String[]]@($_.ImagesUrl)
@@ -27,7 +29,7 @@ $js | Select-Object Title,ImagesUrl,Index | ConvertTo-Json >> $OutFile
 #Note: 破壊的変更で$jsを壊しちゃうので上記の後ろで実行する
 $jsMin = $js | % {
     $_.ImagesUrl = $_.ImagesUrl | % {
-        return $_ -replace "https://mo4koma.iranika.info/4koma/ja"
+        return $_ -replace $ImageDirUrl
     }
     if ($_.ImagesUrl -is [System.String]){
         $_.ImagesUrl = [System.String[]]@($_.ImagesUrl)
