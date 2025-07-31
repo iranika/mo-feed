c2556beeba28940bf559c77930fb0d8efc7eae5d
iranika <7370486+iranika@users.noreply.github.com>
2025-07-31 07:35:35 +0000

    mops: find new update.

--- docs/4komaData.js
+++ docs/4komaData.js
@@ -3420,6 +3420,7 @@ pageData =
     "ImagesUrl": [
       "https://mo-4koma.iranika.info/4koma/ja/374-1.jpg",
       "https://mo-4koma.iranika.info/4koma/ja/374-2.jpg",
+      "https://mo-4koma.iranika.info/4koma/ja/374-3.jpg",
       "https://mo-4koma.iranika.info/4koma/ja/sp.jpg"
     ],
     "Index": 374
