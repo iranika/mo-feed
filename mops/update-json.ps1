7b05ee42662d4054b7b1e3b90b9872fa9eb0d7b6
Aki Kubota <iranica109@gmail.com>
2026-04-28 15:02:27 +0900

    fix: 差分更新の実装

--- mops/update-json.ps1
+++ mops/update-json.ps1
@@ -5,6 +5,8 @@ param (
     $SiteUrl = "http://momoirocode.web.fc2.com/mocode.html"
     ,$OutFile = "4komaData.json"
     ,[switch] $ReturnNewContentOnly
+    ,[switch] $FullFetch
+    ,[int] $ThrottleLimit = 8
 )
 
 Import-Module AngleParse
@@ -36,40 +38,158 @@ class PageData {
     }
 }
 
-if ($ReturnNewContentOnly){
-    #JSON全体を生成せずに最新話だけ出力するオプション
-    $imgs = Invoke-WebRequest $list[-1].href | Select-HtmlContent "img", @{
+function Get-ImagesUrlFromPage {
+    param(
+        [Parameter(Mandatory)]
+        [string] $PageUrl
+    )
+    return Invoke-WebRequest $PageUrl | Select-HtmlContent "img", @{
         src = [AngleParse.Attr]::Src
     } | ? { !([string]$_.src).Contains("counter_img.php?id=50") } | % { $_.src }
+}
+
+if ($ReturnNewContentOnly){
+    #JSON全体を生成せずに最新話だけ出力するオプション
+    $imgs = Get-ImagesUrlFromPage -PageUrl $list[-1].href
     return New-Object PageData($list[-1].Title, $list[-1].href, $imgs)
 }
 
+$existingByUrl = @{}
+if (!$FullFetch -and (Test-Path $OutFile)) {
+    try {
+        $existing = Get-Content -Raw $OutFile | ConvertFrom-Json
+        foreach ($e in @($existing)) {
+            if ($null -ne $e.BaseUrl -and $e.BaseUrl -ne "") {
+                $existingByUrl[$e.BaseUrl] = $e
+            }
+        }
+    } catch {
+        Write-Warning "Failed to read existing json. fall back to FullFetch. file=$OutFile"
+        $FullFetch = $true
+    }
+}
 
-$obj = 0..($list.count -1) | % -parallel {
-    #NOTE: parallelオプションを使うとソートが必要になるので注意
-    #NOTE: $listのindexを取るためにあえて0..MaxLength-1でForEachしている
-    Import-Module AngleParse
-    class PageData {
-        [string] $Title
-        [string] $BaseUrl
-        [string[]] $ImagesUrl
-        PageData([string]$title, [string]$baseUrl, [string[]]$images){
-            $this.Title = $title
-            $this.BaseUrl = $baseUrl
-            $this.ImagesUrl = $images
+if ($FullFetch -or $existingByUrl.Count -eq 0) {
+    $obj = 0..($list.count -1) | % -parallel -ThrottleLimit $ThrottleLimit {
+        #NOTE: parallelオプションを使うとソートが必要になるので注意
+        #NOTE: $listのindexを取るためにあえて0..MaxLength-1でForEachしている
+        Import-Module AngleParse
+        class PageData {
+            [string] $Title
+            [string] $BaseUrl
+            [string[]] $ImagesUrl
+            PageData([string]$title, [string]$baseUrl, [string[]]$images){
+                $this.Title = $title
+                $this.BaseUrl = $baseUrl
+                $this.ImagesUrl = $images
+            }
+        }
+        function Get-ImagesUrlFromPage {
+            param([Parameter(Mandatory)][string] $PageUrl)
+            return Invoke-WebRequest $PageUrl | Select-HtmlContent "img", @{
+                src = [AngleParse.Attr]::Src
+            } | ? { !([string]$_.src).Contains("counter_img.php?id=50") } | % { $_.src }
+        }
+
+        $imgs = Get-ImagesUrlFromPage -PageUrl ($using:list)[$_].href
+        $result = New-Object PageData(($using:list)[$_].title, ($using:list)[$_].href, $imgs)
+        return @{
+            Index = $_
+            Page = $result
+        }
+    } | Sort-Object -Property Index | % { $_.Page }
+} else {
+    # 差分取得: 直近3話だけ取り直して既存にマージする
+    $tailCount = 3
+    $startIndex = [Math]::Max(0, $list.Count - $tailCount)
+    $targets = $startIndex..($list.Count - 1) | % {
+        $i = $_
+        [pscustomobject]@{
+            Index = $i
+            Title = [string]$list[$i].title
+            Href  = [string]$list[$i].href
         }
     }
-    $imgs = Invoke-WebRequest ($using:list)[$_].href | Select-HtmlContent "img", @{
-        src = [AngleParse.Attr]::Src
-    } | ? { !([string]$_.src).Contains("counter_img.php?id=50") } | % { $_.src }
-    
-    $result = New-Object PageData(($using:list)[$_].title, ($using:list)[$_].href, $imgs)
-    Write-Information $result
-    return @{
-        Index = $_
-        Page = $result
+
+    $fetchedByUrl = @{}
+    if ($targets.Count -gt 0) {
+        $fetched = $targets | % -parallel -ThrottleLimit $ThrottleLimit {
+            Import-Module AngleParse
+            class PageData {
+                [string] $Title
+                [string] $BaseUrl
+                [string[]] $ImagesUrl
+                PageData([string]$title, [string]$baseUrl, [string[]]$images){
+                    $this.Title = $title
+                    $this.BaseUrl = $baseUrl
+                    $this.ImagesUrl = $images
+                }
+            }
+            function Get-ImagesUrlFromPage {
+                param([Parameter(Mandatory)][string] $PageUrl)
+                return Invoke-WebRequest $PageUrl | Select-HtmlContent "img", @{
+                    src = [AngleParse.Attr]::Src
+                } | ? { !([string]$_.src).Contains("counter_img.php?id=50") } | % { $_.src }
+            }
+
+            $imgs = Get-ImagesUrlFromPage -PageUrl $_.Href
+            $page = New-Object PageData($_.Title, $_.Href, $imgs)
+            return @{ Url = $_.Href; Page = $page }
+        }
+
+        foreach ($x in @($fetched)) {
+            $fetchedByUrl[[string]$x.Url] = $x.Page
+        }
     }
-} | Sort-Object -Property Index | % { $_.Page }
+
+    $obj = for ($i = 0; $i -lt $list.Count; $i++) {
+        $href = [string]$list[$i].href
+        if ($fetchedByUrl.ContainsKey($href)) {
+            $fetchedByUrl[$href]
+        } elseif ($existingByUrl.ContainsKey($href)) {
+            # Titleだけ更新された場合に追随（ImagesUrlは既存維持）
+            $e = $existingByUrl[$href]
+            $t = [string]$list[$i].title
+            if ($null -ne $t -and $t -ne "" -and $e.Title -ne $t) {
+                $e.Title = $t
+            }
+            $e
+        } else {
+            $null
+        }
+    }
+
+    if (@($obj | ? { $_ -eq $null }).Count -gt 0) {
+        Write-Warning "Diff mode produced gaps (missing entries). fall back to FullFetch."
+        $FullFetch = $true
+        $obj = 0..($list.count -1) | % -parallel -ThrottleLimit $ThrottleLimit {
+            Import-Module AngleParse
+            class PageData {
+                [string] $Title
+                [string] $BaseUrl
+                [string[]] $ImagesUrl
+                PageData([string]$title, [string]$baseUrl, [string[]]$images){
+                    $this.Title = $title
+                    $this.BaseUrl = $baseUrl
+                    $this.ImagesUrl = $images
+                }
+            }
+            function Get-ImagesUrlFromPage {
+                param([Parameter(Mandatory)][string] $PageUrl)
+                return Invoke-WebRequest $PageUrl | Select-HtmlContent "img", @{
+                    src = [AngleParse.Attr]::Src
+                } | ? { !([string]$_.src).Contains("counter_img.php?id=50") } | % { $_.src }
+            }
+
+            $imgs = Get-ImagesUrlFromPage -PageUrl ($using:list)[$_].href
+            $result = New-Object PageData(($using:list)[$_].title, ($using:list)[$_].href, $imgs)
+            return @{
+                Index = $_
+                Page = $result
+            }
+        } | Sort-Object -Property Index | % { $_.Page }
+    }
+}
 
 $obj | ConvertTo-Json | Out-File $OutFile -Encoding utf8
 $obj
